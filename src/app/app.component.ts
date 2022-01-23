0c6377454bdd8571b11c4b73691a94cde9577152
EmilioC <emicorgul@gmail.com>
2022-01-24 00:24:08 +0100

    Test with the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,8 @@ import { Product } from './models/product.model';
 })
 export class AppComponent {
 
-  imgParent = 'https://www.w3schools.com/w3images/avatar5.png';
+  imgParent = 'http://www.carburos.com/~/media/Images/Banners/microsites/instalaciones/telemetria.jpg?la=es-ES';
+  showImg = true;
 
   products : Product [] = [
     {
@@ -16,7 +17,7 @@ export class AppComponent {
       name: 'Regulator 1',
       price: 238,
       description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
-      img: 'https://www.w3schools.com/w3images/avatar5.png',
+      img: 'http://www.carburos.com/~/media/Images/Banners/microsites/instalaciones/servicio-punutal.jpg?h=179&la=es-ES&w=480',
     },
     {
       id: "CM40909400904",
@@ -57,5 +58,10 @@ export class AppComponent {
 
 onLoaded(img:string){
   console.log(img);
+}
+
+toggleImg(){
+  this.showImg = !this.showImg;
+  console.log(this.showImg);
 }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,26 +1,60 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
 
-  heroes = [1,1,1,1,1,1];
+  heroes = [1,1];
 
   imageDefault = "/assets/img/descarga.png";
+  counter = 0;
+  counterFn: number | undefined;
   
   constructor() { 
     // before render
     // No async -- one time for each component only call 1 time
+    // En el constructor debemos ejecutar acciones inmediatas
+    // no acciones que esperen datos (async)
     console.log("Constructor",'imgValue =>', this.img);
   }
 
+  ngOnChanges(){
+    // Before render
+    // The function is update the changes in the inputs
+    // Working many times
+  }
+
   ngOnInit(): void {
+
+    //Berfore render
+    // async - fetch - once time (solo una vez ej: cuando
+    // llamamos a una API y traemos todos los datos)
+    console.log("ngOnInit()",'imgValue =>', this.img);
+
+    this.counterFn= window.setInterval(()=>{
+      this.counter += 1;
+      console.log('--Run counter');
+    },1000)
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log("ngAfterViewInit()",'imgValue =>', this.img);
+  }
+
+  ngOnDestroy(){
+    // delete
+    console.log("ngOnDestroy()",'imgValue =>', this.img);
+    //Cancelamos el evento para que no se quede en 2º plano si 
+    // el componente ya no está en pantalla
+    window.clearInterval(this.counterFn);
   }
 
   imgError(){
