56d30c68e0ccbd3607dcb7f8d89907c8c8739a02
EmilioC <emicorgul@gmail.com>
2022-01-28 23:25:20 +0100

    UCreate the new component product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,50 +11,6 @@ export class AppComponent {
   imgParent = 'http://www.carburos.com/~/media/Images/Banners/microsites/instalaciones/telemetria.jpg?la=es-ES';
   showImg = true;
 
-  products : Product [] = [
-    {
-      id: "CM40909400904",
-      name: 'Regulator 1',
-      price: 238,
-      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
-      img: 'http://www.carburos.com/~/media/Images/Banners/microsites/instalaciones/servicio-punutal.jpg?h=179&la=es-ES&w=480',
-    },
-    {
-      id: "CM40909400904",
-      name: 'Regulator 1',
-      price: 238,
-      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
-      img: 'https://www.w3schools.com/w3images/avatar5.png',
-    },
-    {
-      id: "CM40909400904",
-      name: 'Regulator 1',
-      price: 238,
-      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
-      img: 'https://www.w3schools.com/w3images/avatar5.png',
-    },
-    {
-      id: "CM40909400904",
-      name: 'Regulator 1',
-      price: 238,
-      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
-      img: 'https://www.w3schools.com/w3images/avatar5.png',
-    },
-    {
-      id: "CM40909400904",
-      name: 'Regulator 1',
-      price: 238,
-      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
-      img: 'https://www.w3schools.com/w3images/avatar5.png',
-    },
-    {
-      id: "CM40909400904",
-      name: 'Regulator 1',
-      price: 238,
-      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
-      img: 'https://www.w3schools.com/w3images/avatar5.png',
-    },
-  ];
 
 onLoaded(img:string){
   console.log(img);
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -16,6 +16,50 @@ export class ProductsComponent implements OnInit {
     description: '',
     img: '',
   };
+  products : Product [] = [
+    {
+      id: "CM40909400904",
+      name: 'Regulator 1',
+      price: 238,
+      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
+      img: 'http://www.carburos.com/~/media/Images/Banners/microsites/instalaciones/servicio-punutal.jpg?h=179&la=es-ES&w=480',
+    },
+    {
+      id: "CM40909400904",
+      name: 'Regulator 1',
+      price: 238,
+      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
+      img: 'https://www.w3schools.com/w3images/avatar5.png',
+    },
+    {
+      id: "CM40909400904",
+      name: 'Regulator 1',
+      price: 238,
+      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
+      img: 'https://www.w3schools.com/w3images/avatar5.png',
+    },
+    {
+      id: "CM40909400904",
+      name: 'Regulator 1',
+      price: 238,
+      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
+      img: 'https://www.w3schools.com/w3images/avatar5.png',
+    },
+    {
+      id: "CM40909400904",
+      name: 'Regulator 1',
+      price: 238,
+      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
+      img: 'https://www.w3schools.com/w3images/avatar5.png',
+    },
+    {
+      id: "CM40909400904",
+      name: 'Regulator 1',
+      price: 238,
+      description: 'Carburos Metálicos posee una división especial que puede ayudarle a conseguir cualquier tipo de regulador para su uso en sistemas de una botella o sistemas de distribuciones de gas central, en el punto de uso o en colectores. Hay disponibles reguladores aptos para presiones de hasta 300 bares.',
+      img: 'https://www.w3schools.com/w3images/avatar5.png',
+    },
+  ];
 
   constructor() { }
 
